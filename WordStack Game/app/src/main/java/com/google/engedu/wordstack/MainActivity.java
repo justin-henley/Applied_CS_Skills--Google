5e768451ba5ac52a60d96dc59223a26baa984dbc
justin-henley <justin.a.henley@gmail.com>
2020-12-16 02:07:03 +0800

    Implemented extension to check correctness of user entry at end of round

--- WordStack Game/app/src/main/java/com/google/engedu/wordstack/MainActivity.java
+++ WordStack Game/app/src/main/java/com/google/engedu/wordstack/MainActivity.java
@@ -41,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
     public static final int LIGHT_BLUE = Color.rgb(176, 200, 255);
     public static final int LIGHT_GREEN = Color.rgb(200, 255, 200);
     private ArrayList<String> words = new ArrayList<>();
+    private ArrayList<String> fullDictionary = new ArrayList<>();
     private Random random = new Random();
     private StackedLayout stackedLayout;
     private String word1, word2;
@@ -57,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
             String line;
             while((line = in.readLine()) != null) {
                 String word = line.trim();
+                fullDictionary.add(word);
                 if (word.length() == WORD_LENGTH) {
                     words.add(word);
                 }
@@ -86,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
                 tile.moveToViewGroup((ViewGroup) v);
                 if (stackedLayout.empty()) {
                     TextView messageBox = findViewById(R.id.message_box);
-                    messageBox.setText(word1 + " " + word2);
+                    messageBox.setText(String.format(getString(R.string.display_originals), word1, word2));
                 }
                 placedTiles.push(tile);
                 return true;
@@ -122,7 +124,8 @@ public class MainActivity extends AppCompatActivity {
                     tile.moveToViewGroup((ViewGroup) v);
                     if (stackedLayout.empty()) {
                         TextView messageBox = findViewById(R.id.message_box);
-                        messageBox.setText(word1 + " " + word2);
+                        messageBox.setText(String.format(getString(R.string.display_originals), word1, word2));
+                        checkAnswer();
                     }
                     placedTiles.push(tile);
                     return true;
@@ -139,6 +142,7 @@ public class MainActivity extends AppCompatActivity {
         word2LinearLayout.removeAllViews();
         stackedLayout.clear();
         placedTiles.clear();
+        
 
         TextView messageBox = findViewById(R.id.message_box);
         messageBox.setText(R.string.game_start);
@@ -148,6 +152,25 @@ public class MainActivity extends AppCompatActivity {
         word1 = words.get(random.nextInt(words.size() - 1));
         word2 = words.get(random.nextInt(words.size() - 1));
 
+        shuffleAndReverse();
+
+        return true;
+    }
+
+    public boolean onUndo(View view) {
+        if (!placedTiles.empty() && !stackedLayout.empty()) {
+            LetterTile tile = placedTiles.pop();
+            tile.moveToViewGroup(stackedLayout);
+            return true;
+        }
+        else
+            return false;
+    }
+
+    // Shuffles together the two strings, preserving their orders relative to their words
+    // Returns the shuffled string in reverse order for pushing onto a stack
+    // Called recursively until a sufficiently shuffled string is returned
+    private void shuffleAndReverse() {
         // shuffle words into a single mixed string, with word orders preserved
         StringBuilder shuffled = new StringBuilder();
         int count1 = 0, count2 = 0;
@@ -161,24 +184,54 @@ public class MainActivity extends AppCompatActivity {
             shuffled.append(word2.charAt(count2++));
         }
 
-        // Push each letter onto stackedLayout as a LetterTile in reverse order
-        shuffled.reverse();
-        for (int i = 0; i < shuffled.length(); i++) {
-            LetterTile tile = new LetterTile(this, shuffled.charAt(i));
-            stackedLayout.push(tile);
+        // Check if shuffled
+        if (shuffled.indexOf(word1) != -1 || shuffled.indexOf(word2) != -1) {
+            shuffleAndReverse();
         }
-
-        return true;
+        else {
+            // Push each letter onto stackedLayout as a LetterTile in reverse order
+            shuffled.reverse();
+            for (int i = 0; i < shuffled.length(); i++) {
+                LetterTile tile = new LetterTile(this, shuffled.charAt(i));
+                stackedLayout.push(tile);
+            }
+        }
     }
 
-    public boolean onUndo(View view) {
-        if (!placedTiles.empty() && !stackedLayout.empty()) {
-            LetterTile tile = placedTiles.pop();
-            tile.moveToViewGroup(stackedLayout);
-            return true;
-        }
-        else
-            return false;
 
+    // tells the user if they found good words
+    private void checkAnswer() {
+        // Build answer strings from the viewgroups
+        StringBuilder build1 = new StringBuilder();
+        StringBuilder build2 = new StringBuilder();
+        String answer1, answer2, toastText;
+
+        ViewGroup v1 = findViewById(R.id.word1);
+        ViewGroup v2 = findViewById(R.id.word2);
+
+        for (int i = 0; i < v1.getChildCount(); i++) {
+            build1.append(((LetterTile) v1.getChildAt(i)).getText());
+        }
+        for (int i = 0; i < v2.getChildCount(); i++) {
+            build2.append(((LetterTile) v2.getChildAt(i)).getText());
+        }
+
+        answer1 = build1.toString();
+        answer2 = build2.toString();
+
+        // Check if the answers are in dictionary
+        if ((answer1.compareTo(word1) == 0 || answer1.compareTo(word2) == 0)
+                && (answer2.compareTo(word1) == 0 || answer2.compareTo(word2) == 0)) {
+            toastText = "Perfect match!";
+        }
+        else if (fullDictionary.contains(answer1) && fullDictionary.contains(answer2)) {
+            toastText = "Not the words we wanted, but the words we deserved.";
+        }
+        else {
+            toastText = "Not even close!";
+        }
+
+        Toast toast = Toast.makeText(this, toastText, Toast.LENGTH_LONG);
+        toast.show();
     }
 }
